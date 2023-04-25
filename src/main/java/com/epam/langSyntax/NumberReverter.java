4ed96651342d77d86195616ea450005fbc4a6bc4
Emil <mr.emilka0@gmail.com>
2023-04-26 03:11:29 +0600

    LangSyntax tasks solved

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -1,8 +1,11 @@
 package com.epam.langSyntax;
 
 public class NumberReverter {
-
     public void revert(int number) {
+        int reversedNumber = number % 10 * 100;
+        number /= 10;
+        reversedNumber += number % 10 * 10;
+        reversedNumber += number / 10;
+        System.out.println(reversedNumber);
     }
-
 }
